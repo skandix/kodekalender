6cbec7c43ff6d62a72a7ec20d3c94d48093d856e
Bendik LAmbda <bendik@datapor.no>
2021-12-01 14:54:21 +0100

    knowit
    Signed-off-by: Bendik LAmbda <bendik@datapor.no>

--- 2021/aoc/day0/main.py
+++ 2021/aoc/day0/main.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
 
-with open("./input", "r") as inp:
-    print(inp.read())
 
-print("Helloo")
+class day1:
+    def __init__(self):
+        self.test_file = open("./test", "r")
+        self.test_list = self.ToList(self.test_file)
+
+        self.input_file = open("./input", "r")
+        self.input_list = self.ToList(self.input_file)
+
+        self.input_result = list()
+
+    def task1(self):
+        sel_list = self.input_list
+        ...
+
+    def task2(self):
+        sel_list = self.input_list
+        ...
+
+    def ToList(self, input: list[int]) -> list[int]:
+        return [(x.strip()) for x in input]
+
+
+d1 = day1()
+
+d1.task1()
+d1.task2()
